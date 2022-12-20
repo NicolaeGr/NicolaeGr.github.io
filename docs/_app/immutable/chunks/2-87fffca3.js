4b449387235db16402633e45f1a83fc2ea4e7dfa
NicolaeGr <nicolaegr@proton.me>
2022-12-20 17:28:55 +0200

    innit

---
+++ docs/_app/immutable/chunks/2-87fffca3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6c2edae8.js";export{t as component};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<div class="container mx-auto my-0 pt-8 text-center">
+	<h1 class="pt-1 text-4xl">Hi!</h1>
+	<h3 class="pt-1 text-2xl">I'm Nicolae Grosu</h3>
+	<p class="pt-1 text-xl">This is my future portofolio website</p>
+</div>
