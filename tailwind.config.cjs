85a218707af6d6d39b39bb9c109abd17a0872223
NicolaeGr <nicolaegr@proton.me>
2023-02-08 17:15:49 +0200

    added page colors and installed web-vitals

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			'main-purple': '#7355E7',
+			'main-dark': '#000000',
+			'secundary-dark': '#080D1D',
+			'text-gray-1': '#414141',
+			'text-gray-2': '#767676'
+		}
 	},
 	plugins: []
 };
