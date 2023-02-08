fea136e61ce0930dcad76c21d7a3a72c3b6e01ac
NicolaeGr <nicolaegr@proton.me>
2023-02-08 17:16:22 +0200

    added header bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
-<div class="container mx-auto my-0 pt-8 text-center">
-	<h1 class="pt-1 text-4xl">Hi!</h1>
-	<h3 class="pt-1 text-2xl">I'm Nicolae Grosu</h3>
-	<p class="pt-1 text-xl">This is my future portofolio website</p>
-</div>
+<svelte:head>
+	<title>Grosu Nicolae</title>
+	<meta name="description" content="Freelance Web Developer from Moldova" />
+</svelte:head>
+
+<header class="h-16 w-screen">
+	<div class="container m-auto grid h-full w-full grid-cols-6 grid-rows-1">
+		<div class="col-span-1 mx-16 my-auto hidden md:inline">GN</div>
+		<div class="col-span-4 flex items-center justify-center gap-8">
+			<a href="#about">About Me</a>
+
+			<a href="#projects">My Projects</a>
+
+			<a href="#reviews">Reviews</a>
+		</div>
+		<div class="col-span-1" />
+	</div>
+</header>
+
+<section id="hero" />
+
+<section id="about" />
+
+<section id="projects" />
+
+<section id="reviews" />
+<footer />
