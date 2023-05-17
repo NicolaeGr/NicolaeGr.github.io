1fb34411c0ddf7eb1bb0a5edb61a54f02b7c210c
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2023-05-17 13:37:42 +0300

    Added Vite Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
 	import '../app.css';
 
 	function scrollIntoView(event: MouseEvent): void {
