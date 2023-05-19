89fd5b002f5d0ba244a13a4b24e2d12070a6f98e
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2023-05-19 10:13:58 +0300

    added vite plugin for image compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ViteImageOptimizer()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
+	}
 };
 
 export default config;
